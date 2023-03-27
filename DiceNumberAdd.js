1ee1bb5e50920671022b242cbf983d79cf066220
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-27 10:16:27 +0530

    Added: Sequence Programs

---
+++ DiceNumberAdd.js
@@ -0,0 +1,5 @@
+//use Random to get Dice Number between 1 to 6
+
+let dice1 = Math.floor(Math.random() * 6)+1;  
+let dice2 = Math.floor(Math.random() * 6)+1;  
+console.log("Addition of 2 random dice numbers : "+(dice1+dice2));
---
+++ RandomSingleDigit.js
@@ -0,0 +1,4 @@
+//Random Function Math.floor(Math.random() * 10); to get Single Digit.
+
+let singleDigit = Math.floor(Math.random() * 10);  
+console.log("Random single digit : "+SingleDigit);
